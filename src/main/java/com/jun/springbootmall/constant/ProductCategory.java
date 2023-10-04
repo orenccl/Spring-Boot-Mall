99e0392e5ff2f1eaa5f580c713c34ce68a284e36
jun <junliang.chen@iisigroup.com>
2023-10-04 13:57:33 +0800

    [+] Product - Category function

---
+++ src/main/java/com/jun/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.jun.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    BOOK
+}
--- src/main/java/com/jun/springbootmall/model/Product.java
+++ src/main/java/com/jun/springbootmall/model/Product.java
@@ -1,11 +1,13 @@
 package com.jun.springbootmall.model;
 
+import com.jun.springbootmall.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
     Integer productId;
     String productName;
-    String category;
+    ProductCategory category;
     String imageUrl;
     Integer price;
     Integer stock;
@@ -29,11 +31,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
