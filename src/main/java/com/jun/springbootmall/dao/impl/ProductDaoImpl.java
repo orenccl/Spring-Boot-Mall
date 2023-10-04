81dd84e9f96b38cd69ceed995b9d93f9b861d951
jun <junliang.chen@iisigroup.com>
2023-10-04 17:38:55 +0800

    [+] Product - Extract filtering sql

--- src/main/java/com/jun/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/jun/springbootmall/dao/impl/ProductDaoImpl.java
@@ -27,17 +27,8 @@ public class ProductDaoImpl implements ProductDao {
         String sql = "SELECT count(*) FROM product WHERE 1 = 1";
 
         Map<String, Object> map = new HashMap<>();
-
         // Filter
-        if (productQueryParams.getCategory() != null) {
-            sql += " AND category = :category";
-            map.put("category", productQueryParams.getCategory().name());
-        }
-
-        if (productQueryParams.getSearch() != null) {
-            sql += " AND product_name LIKE :search";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         return namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
     }
@@ -49,17 +40,8 @@ public class ProductDaoImpl implements ProductDao {
                 "FROM product WHERE 1 = 1";
 
         Map<String, Object> map = new HashMap<>();
-
         // Filter
-        if (productQueryParams.getCategory() != null) {
-            sql += " AND category = :category";
-            map.put("category", productQueryParams.getCategory().name());
-        }
-
-        if (productQueryParams.getSearch() != null) {
-            sql += " AND product_name LIKE :search";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         // JDBC OrderBy and Sort not working with paramMap
         // Sort
@@ -141,5 +123,19 @@ public class ProductDaoImpl implements ProductDao {
         map.put("productId", productId);
 
         namedParameterJdbcTemplate.update(sql, map);
+    }
+
+    private String addFilteringSql(String sql, Map<String, Object> map, ProductQueryParams productQueryParams) {
+        if (productQueryParams.getCategory() != null) {
+            sql += " AND category = :category";
+            map.put("category", productQueryParams.getCategory().name());
+        }
+
+        if (productQueryParams.getSearch() != null) {
+            sql += " AND product_name LIKE :search";
+            map.put("search", "%" + productQueryParams.getSearch() + "%");
+        }
+
+        return sql;
     }
 }
